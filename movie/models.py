11bfbaf25457404f78392fe4dc6b9818d89d562c
RED <purred@udid.co.kr>
2018-11-23 18:22:35 +0900

    최초 작업

--- movie/models.py
+++ movie/models.py
@@ -13,4 +13,7 @@ class Movie(models.Model):
     name = models.CharField(max_length=60)
     img_url = models.URLField()
     release_date = models.DateField()
-    #search_result = models.ForeignKey(SearchResult, on_delete=models.)
+    search_result = models.ForeignKey(SearchResult, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}({})'.format(self.name, self.release_date)
